a1fb84f23f67cd22a7313cc48e0ab5fafd63e1b8
Turiiya <34311583+ttytm@users.noreply.github.com>
2024-06-28 12:43:30 +0200

    fonts: add outstanding install script updates

--- scripts/install_fonts.sh
+++ scripts/install_fonts.sh
@@ -1,25 +1,42 @@
 #!/bin/bash
 
-FONT_DIR="$HOME/.local/share/fonts"
+# Shell script for GNU/Linux systems to download fonts directly from their source.
 
-if [ ! -d "$FONT_DIR" ]; then
-	mkdir "$FONT_DIR"
+local_share_dir="$HOME/.local/share"
+
+if [ ! -d "$local_share_dir" ]; then
+	echo "The script is not intended for the current operating system."
+	exit 2
 fi
 
-cd "$FONT_DIR"
+fonts_dir="$local_share_dir/fonts"
 
-# Nerd Fonts
-# Requires `subversion`
-svn export https://github.com/ryanoasis/nerd-fonts/trunk/patched-fonts/JetBrainsMono/Ligatures JetBrainsNFM 
-svn export https://github.com/ryanoasis/nerd-fonts/trunk/patched-fonts/FantasqueSansMono FantasqueSansNFM
-
-# Regular Fonts
-mkdir FantasqueSansMono && cd FantasqueSansMono \
+# Regular Fonts.
+mkdir -p "$fonts_dir/FantasqueSansMono" \
 	&& curl -LO https://github.com/belluzj/fantasque-sans/releases/latest/download/FantasqueSansMono-LargeLineHeight.tar.gz \
-	&& tar -zxf FantasqueSansMono-LargeLineHeight.tar.gz TTF \
-	&& mv TTF/*.ttf ./ \
-	&& rm -r TTF && rm FantasqueSansMono-LargeLineHeight.tar.gz \
+	&& tar -C "$fonts_dir/FantasqueSansMono" -xvf FantasqueSansMono-LargeLineHeight.tar.gz TTF \
+	&& rm FantasqueSansMono-LargeLineHeight.tar.gz
 
+# Nerd Fonts (requires `subversion`).
+if ! which svn >/dev/null 2>&1; then
+	echo "Nerd Font installation requires subversion to be installed. Make sure 'svn' is executable."
+	exit 1
+fi
+
+nerd_fonts_dir="$fonts_dir/NerdFonts"
+if [ ! -d "$nerd_fonts_dir" ]; then
+	mkdir "$nerd_fonts_dir"
+fi
+
+# FIXME: github dropped support for subversion.
+# Using the package manager is the preferred way to manage font installations.
+nerd_fonts_url="https://github.com/ryanoasis/nerd-fonts/trunk/patched-fonts/"
+svn export "$nerd_fonts_url/JetBrainsMono/Ligatures" "$nerd_fonts_dir/JetBrainsMono"
+svn export "$nerd_fonts_url/VictorMono" "$nerd_fonts_dir/VictorMono"
+svn export "$nerd_fonts_url/Hasklig" "$nerd_fonts_dir/Hasklig"
+svn export "$nerd_fonts_url/FantasqueSansMono" "$nerd_fonts_dir/FantasqueSansMono"
+
+# Update font cache.
 fc-cache -fv
 
 exit 0
